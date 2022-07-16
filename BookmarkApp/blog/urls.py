f82aaf31c285b1f308b985367dd7d0ba828a8004
sunwise <sunwise9634@gmail.com>
2022-07-16 16:03:14 +0900

    MOD: urls.py 수정 (url 추가)

--- BookmarkApp/blog/urls.py
+++ BookmarkApp/blog/urls.py
@@ -3,7 +3,28 @@ from blog import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.PostLV_as_view(), name='index'),
+    # Example: /blog/
+    path('', views.PostLV.as_view(), name='index'),
+
+    # Example: /blog/post/
     path('post/', views.PostLV.as_view(), name='post_list'),
+
+    # Example: /blog/post/django-example/
+    # URL을 좀 더 제한하여 지정하고 싶다면, re_path() 함수를 사용할 수 있다.
     re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
+
+    # Example: /blog/archive/
+    path('archive/', views.PostAV.as_view(), name='post_archive'),
+
+    # Example: /blog/archive/2019
+    path('archive/<int:year>', views.PostYAV.as_view(), name='post_year_archive'),
+
+    # Example: /blog/archive/2019/nov/
+    path('archive/<int:year>/<str:month>/', views.PostMAV.as_view(), name='post_month_archive'),
+
+    # Example: /blog/archive/2019/nov/10
+    path('archive/<int:hear>/<str:month>/<int:day>/', views.PostDAV.as_view(), name='post_day_archive'),
+
+    #Example: /blog/archive/today/
+    path('archive/today/', views.PostTAV.as_view(), name='post_today_archive'),
 ]
