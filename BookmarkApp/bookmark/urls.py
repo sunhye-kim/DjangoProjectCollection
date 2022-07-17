90597840fcd12a3a4e8318a97e471becafdfd6bf
sunwise <sunwise9634@gmail.com>
2022-07-17 21:01:40 +0900

    MOD: 오타수정

--- BookmarkApp/bookmark/urls.py
+++ BookmarkApp/bookmark/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from bookmark.views import BookmarkLV, BookmarkDV
 
 app_name = 'bookmark'
-urlpatters = [
+urlpatterns = [
     path('', BookmarkLV.as_view(), name='index'),
-    path('<int:pk>', BookmarkDV.as_view(), name='detail'),
+    path('<int:pk>/', BookmarkDV.as_view(), name='detail'),
 ]
